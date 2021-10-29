c47f8173a984e68d8ac34a0d7535b0a1a9977d9e
Alexey <91087810+AI-Kusu@users.noreply.github.com>
2021-10-29 17:34:15 +0500

    Add files via upload

---
+++ HistogramTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var date = 31;
+            var dates = new string[date];
+            for (int i = 0; i < date; i++)
+            {
+                dates[i] = (i + 1).ToString();
+            }
+
+            var numberOfBirths = new double[date];
+            foreach (var person in names)
+            {
+                if (person.Name == name && person.BirthDate.Day != 1)
+                    numberOfBirths[person.BirthDate.Day - 1]++;
+            }
+
+
+
+            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), dates, numberOfBirths);
+        }
+    }
+}
